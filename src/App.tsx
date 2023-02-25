1981b23a5679ff0fdd93874df7e9610cb83a2460
Emerson Dantas <emerson.dantaspereira@hotmail.com>
2023-02-25 11:18:58 -0300

    Created vite project with Typescript

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import './styles/global.css';
+import './lib/dayjs';
+import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
+import { api } from './lib/axios';
+
+// import { Habit } from "./components/Habit";
+
+/*window.Notification.requestPermission(permission => {
+  if (permission == 'granted') {
+    new window.Notification('Habits', {
+      body: 'Texto',
+    })
+  }
+}) */ // Local Notification => Não Scheduling / App fechado
+
+navigator.serviceWorker.register('service-worker.js')
+  .then(async serviceWorker => {
+    let subscription = await serviceWorker.pushManager.getSubscription()
+
+    if (!subscription) {
+      const publicKeyResponse = await api.get('/push/public_key')
+
+      subscription = await serviceWorker.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: publicKeyResponse.data.publicKey,
+      })
+    }
+
+    await api.post('/push/register', {
+      subscription,
+    })
+
+    await api.post('/push/send', {
+      subscription,
+    })
+  })
+
+export function App() {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16 items-center'>
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
+  )
+}
+
+export default App
+
+// Componente: Reaproveitar / isolar
+// Propriedade: Uma informação enviada pra modificar um componente visual ou comportamentalmente
